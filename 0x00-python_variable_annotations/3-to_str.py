15f822e0ee3839a8b70bd84503a269eb3398ecce
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:32:10 +0200

    add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This task contains annotated to_str function that returns the string
+representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of a float"""
+    return str(n)
