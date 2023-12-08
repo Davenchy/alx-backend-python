f06619b275e534b788812af9fb05e44e318379a8
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:56:39 +0200

    update: task 6: use Union type to define mixed List type

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This task has a function that takes a list of mixed arguments
 floats and integers and returns their sum as a float"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """returns the sum of a list of floats and integers"""
     return sum(mxd_lst)
