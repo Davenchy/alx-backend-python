9c5359b1a9a6622ed596a82c25f3828363f68557
Fadi Asaad <firon1222@gmail.com>
2023-12-08 03:11:48 +0200

    update: the fucking task 7: use the fucking union shit

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This task contains an annotated function that takes k and v and returns
 a tuple of k and v squared"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple of k and v squared"""
-    return k, v**2
+    return (k, v**2)
