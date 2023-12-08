8c4d123fd5a7a8278a84d57e78c75b2100e27c75
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:19:30 +0200

    add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This task contains the annotated function concat that takes two strings
+and concatenates them and returns the result back"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings"""
+    return str1 + str2
