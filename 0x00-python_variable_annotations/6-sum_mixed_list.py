2a559ae95fffffdffc7cbfdf1015be3a5f8acc95
Fadi Asaad <firon1222@gmail.com>
2023-12-08 03:00:37 +0200

    update: task 6: ALX need to tast Egyptian 45ra

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@ floats and integers and returns their sum as a float"""
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns the sum of a list of floats and integers"""
     return sum(mxd_lst)
