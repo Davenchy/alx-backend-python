fbe2c633cae3654ca975de9633d876287784f1f3
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:24:52 +0200

    add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This task contains the annoatated floor function that returns the floor
+value of a float"""
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    return fl(n)
