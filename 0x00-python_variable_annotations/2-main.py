702a4b430f57070cb394dbb05dfe2116947d5e7a
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:31:53 +0200

    fix: task 2: fix main file of this task

--- 0x00-python_variable_annotations/2-main.py
+++ 0x00-python_variable_annotations/2-main.py
@@ -4,8 +4,8 @@ import math
 
 floor = __import__('2-floor').floor
 
-and = floor(3.14)
+ans = floor(3.14)
 
-print(and == math.floor(3.14))
+print(ans == math.floor(3.14))
 print(floor.__annotations__)
-print("floor(3.14) returns {}, which is a {}".format( and , type( and )))
+print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
