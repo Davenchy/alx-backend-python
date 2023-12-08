e2a4c7f56b012ca8d051f74350eae54d70e4d30a
Fadi Asaad <firon1222@gmail.com>
2023-12-08 02:43:06 +0200

    update: task 5: don't import any module and use sum to solve the problem, I hope that makes the checker happy

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This task contains a function that returns the sum of a list of floats"""
-from functools import reduce
 
 
 def sum_list(input_list: list[float]) -> float:
     """returns the sum of a list of floats"""
-    return reduce(lambda x, y: x + y, input_list, 0)
+    return sum(input_list)
