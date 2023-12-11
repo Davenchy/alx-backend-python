a53616eff234d2949d572ad2e1949b79e499d613
Fadi Asaad <firon1222@gmail.com>
2023-12-11 23:16:29 +0200

    use random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """generates a random value between 0 and max_delay and uses it as a delay"""
-from random import random
 from asyncio import sleep
+from random import uniform
 
 
 async def wait_random(max_delay: float = 10) -> float:
     """generates a random value between 0 and max_delay and uses it as a delay
     after delay returns the value"""
-    delay = random() * max_delay
+    delay = uniform(0, max_delay)
     await sleep(delay)
     return delay
