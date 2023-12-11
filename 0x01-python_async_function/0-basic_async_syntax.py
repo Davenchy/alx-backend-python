5ffc5eb1e1a014ff4963e0cb7c8c17b61c031430
Fadi Asaad <firon1222@gmail.com>
2023-12-11 23:11:53 +0200

    add return type hint annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ from random import random
 from asyncio import sleep
 
 
-async def wait_random(max_delay: float = 10):
+async def wait_random(max_delay: float = 10) -> float:
     """generates a random value between 0 and max_delay and uses it as a delay
     after delay returns the value"""
     delay = random() * max_delay
