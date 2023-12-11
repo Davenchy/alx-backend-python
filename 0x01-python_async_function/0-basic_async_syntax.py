9e9de5d2665c1be558f9424413d404bf71607051
Fadi Asaad <firon1222@gmail.com>
2023-12-11 23:21:36 +0200

    add task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """generates a random value between 0 and max_delay and uses it as a delay"""
-import asyncio
-import random
+from asyncio import sleep
+from random import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """generates a random value between 0 and max_delay and uses it as a delay
     after delay returns the value"""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
+    delay = random() * max_delay
+    await sleep(delay)
     return delay
