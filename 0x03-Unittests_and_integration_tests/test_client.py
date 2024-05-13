487488e111c2d80b92508369718bb30dd943e6b8
Fadi Asaad <firon1222@gmail.com>
2024-05-13 22:13:05 +0300

    add  task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Testing client.py """
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import client
 from client import GithubOrgClient
 
@@ -19,3 +19,17 @@ class TestGithubOrgClient(unittest.TestCase):
         obj.org
         mock.assert_called_once_with(
                 f'https://api.github.com/orgs/{test_org}')
+
+    def test_public_repos_url(self):
+        """ Testing _public_repos_url property  """
+        PAYLOAD = 'a7a'
+        with patch(
+                'client.GithubOrgClient.org',
+                new_callable=PropertyMock) as mock:
+            mock.return_value = {
+                "repos_url": PAYLOAD,
+            }
+            obj = GithubOrgClient('another a7a')
+            self.assertEqual(obj._public_repos_url, PAYLOAD)
+
+
